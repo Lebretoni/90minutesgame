a8b86689c78c2edb655d15f5a48041a0efa2e757
Lebretoni <60005137+Lebretoni@users.noreply.github.com>
2020-03-17 11:29:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// PREP we need to make an onlick event when the buttons are clicked
+//
+let button1 = document.getElementById('Rock')
+let button2 = document.getElementById('paper')
+let button3 = document.getElementById('Scissors')
+let button4 = document.getElementById('Lizard')
+let button5 = document.getElementById('Spock')
